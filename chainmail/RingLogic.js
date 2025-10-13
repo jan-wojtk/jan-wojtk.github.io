07bb6bac0ad0d3f09d42a42dbaba0d0817b4eaa6
jannie-wojtk <jannie.wojtk@gmail.com>
2025-10-13 00:22:30 -0400

    Fixed Apply On Click

--- chainmail/RingLogic.js
+++ chainmail/RingLogic.js
@@ -9,18 +9,18 @@ class RingLogic {
         const template = this.#getTemplate();
         const ring = this.#parser.parseFromString(template, 'text/html').body.children[0];
         
-        ring.onclick = this.#onClick;
-        ring.oncontextmenu = this.#onContextMenu;
+        ring.onclick = RingLogic.#onClick;
+        ring.oncontextmenu = RingLogic.#onContextMenu;
         
         return ring;
     }
     
-    #onClick(event) {
+    static #onClick(event) {
         var ringContainer = event.target.closest('.ring-container');
-        ringContainer.style['border-color'] = _getColorPickerSelectElement().value;
+        ringContainer.style['border-color'] = RingLogic.#getColorPickerPreviewElement().value;
     }
     
-    #onContextMenu(event) {
+    static #onContextMenu(event) {
         event.preventDefault();
         var ringContainer = event.target.closest('.ring-container');
         ringContainer.style['border-color'] = '';
@@ -35,5 +35,11 @@ class RingLogic {
                 <div class="ring-partial ring-partial_bottom"><div class="ring"></div></div>
             </div>    
         `;
+    }
+    
+    // avoid ciclical depedencies by directly accessing this form element here
+    // todo: figure out how to avoid this
+    static #getColorPickerPreviewElement() {
+        return document.getElementById('color-picker_preview');
     }
 }
